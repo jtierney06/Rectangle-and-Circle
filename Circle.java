137d93163b513297fae6c6dd94b31bed666fadd1
jtierney06 <jamespt06@icloud.com>
2024-02-27 14:23:03 -0500

    .

---
+++ Circle.java
@@ -0,0 +1,11 @@
+public class Circle
+{
+
+        public static void main(String[] args){
+            double radius = 4.28;
+            double area = Math.PI * radius * radius;
+            double circumference = 2 * Math.PI * radius;
+            System.out.println("The area is " + area);
+            System.out.println("The circumference is " + circumference);
+        }
+    }
